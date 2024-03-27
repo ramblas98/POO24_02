e7d3d25bc4e149b32730c1510a21b34904395d96
Admin <Admin@Tania>
2024-03-27 16:21:04 -0300

    primera

---
+++ TP1_EJE15/src/tp1_eje15/TP1_EJE15.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_eje15;
+
+/**
+ *
+ * @author Admin
+ */
+public class TP1_EJE15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado=new Scanner(System.in);
+        int S;
+        String N;
+        S=teclado.nextInt();
+        N=teclado.next();
+        
+    }
+    
+}
