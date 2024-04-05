9cc0375990c6e1a0ea9d44874a0e4859e05c334f
magal <magal@AT560>
2024-04-05 17:14:55 -0300

    ejercicio 1 funcional

--- TP2_EJ1/src/tp2_ej1/Complejo.java
+++ TP2_EJ1/src/tp2_ej1/Complejo.java
@@ -9,5 +9,96 @@ package tp2_ej1;
  * @author magal
  */
 public class Complejo {
+    private double re, im;
+
+    public Complejo() {
+        this.re = 0;
+        this.im = 0;
+    }
+
+    public double getRe() {
+        return re;
+    }
+
+    public double getIm() {
+        return im;
+    }
+
+    public void setRe(int re) {
+        this.re = re;
+    }
+
+    public void setIm(int im) {
+        this.im = im;
+    }
     
+    //Métodos de instancia(NO estáticos)
+    
+    public void inc(Complejo a){
+        this.re += a.re;
+        this.im += a.im;
+    }
+    
+    public void dec(Complejo a){
+        this.re -= a.re;
+        this.im -= a.im;
+    }
+    
+    public void por(Complejo a){ //(c.re*a.re + c.im*a.im , c.re*a.im - c.im*a.re) 
+        double p1, p2;
+        p1 = this.re * a.im;
+        p2 = this.im * a.re;
+        this.re = this.re * a.re + this.im * a.im;
+        this.im = p1 - p2;
+    }
+    
+    public void div(Complejo a){
+        this.re = (this.re * a.re + this.im * a.im) / (Math.pow(a.re,2) + Math.pow(a.im, 2));
+        this.im = (this.im * a.re - this.re * a.im) / (Math.pow(a.re,2) + Math.pow(a.im, 2));
+    }
+    
+    public double modulo(){
+        return (Math.sqrt((Math.pow(this.re,2)) + Math.pow(this.im,2)));
+    }
+    
+    public Complejo conjugado(){
+        this.im = (-1) * this.im;
+        return this;
+    }
+    
+    //Métodos Generales
+
+    @Override
+    public String toString() {
+        return "Complejo{" + "re=" + re + ", im=" + im + '}';
+    }
+    
+    //Métodos de Clases
+    
+    public static Complejo suma(Complejo a, Complejo b){
+        Complejo c = new Complejo();
+        c.re = a.re + b.re;
+        c.im = a.im + b.im;
+        return c;
+    }
+    
+    public static Complejo resta(Complejo a, Complejo b){
+        Complejo c = new Complejo();
+        c.re = a.re - b.re;
+        c.im = a.im - b.im;
+        return c;
+    }
+    
+    public static Complejo priducto(Complejo a, Complejo b){
+        Complejo c = new Complejo();
+        c.re = a.re * b.re + a.im * b.im;
+        c.im = a.re * b.im - a.im * b.re;
+        return c;
+    }
+    public static Complejo division(Complejo a, Complejo b){
+        Complejo c = new Complejo();
+        c.re = (a.re * b.re + a.im * b.im) / (Math.pow(b.re, 2) + Math.pow(b.im, 2));
+        c.im = (a.im * b.re - a.re * b.im) / (Math.pow(b.re, 2) + Math.pow(b.im, 2));
+        return c;
+    }
 }
--- TP2_EJ1/src/tp2_ej1/TP2_EJ1.java
+++ TP2_EJ1/src/tp2_ej1/TP2_EJ1.java
@@ -4,9 +4,11 @@
  */
 package tp2_ej1;
 
+import java.util.Scanner;
+
 /**
  *
- * @author magal
+ * @author magali.17.reina@gmail.com
  */
 public class TP2_EJ1 {
 
@@ -14,7 +16,70 @@ public class TP2_EJ1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        Complejo c = new Complejo();
+        Complejo d = new Complejo();
+        double e;
+        Complejo f = new Complejo();
+        
+        System.out.println("Pimer complejo:");
+        
+        System.out.print("Parte Real: ");
+        c.setRe(teclado.nextInt());
+        System.out.print("Parte Compleja: ");
+        c.setIm(teclado.nextInt());
+        //System.out.println("Pimer complejo ingresado: (" + c.getRe() + " , " + c.getIm() + "i)");
+        System.out.println(c);
+        
+        System.out.println("Segundo complejo:");
+        d.setRe(5);
+        d.setIm(7);
+        System.out.println(d);
+        System.out.println();
+        System.out.println("Resultado de la SUMA:");
+        c.inc(d);
+        System.out.println(c);
+        System.out.println();
+        System.out.println("Resultado de la DIFERENCIA:");
+        c.dec(d);
+        System.out.println(c);
+        System.out.println();
+        System.out.println("Resultado del PRODUCTO:");
+        c.por(d);
+        System.out.println(c);
+        System.out.println();
+        System.out.println("Resultado del COCIENTE:");
+        c.div(d);
+        System.out.println(c);
+        System.out.println();
+        System.out.println("Resultado del MODULO:");
+        e = c.modulo();
+        System.out.println(e);
+        System.out.println();
+        System.out.println("Resultado del CONJUGADO");
+        f = c.conjugado();
+        System.out.println(f);
+        System.out.println();
+        Complejo g = new Complejo();
+        Complejo h = new Complejo();
+        g.setRe(3);
+        g.setIm(1);
+        System.out.println(g);
+        System.out.println();
+        h.setRe(2);
+        h.setIm(5);
+        System.out.println(h);
+        System.out.println();
+        
+        
+        System.out.println("Resultado de la SUMA:");
+        System.out.println(Complejo.suma(g,h));
+        System.out.println("Resultado de la DIFERNECIA:");
+        System.out.println(Complejo.resta(g, h));
+        System.out.println("Resultado del PRODUCTO:");
+        System.out.println(Complejo.priducto(g, h));
+        System.out.println("Resultado del COCIENTE:");
+        System.out.println(Complejo.division(g, h));
     }
     
 }
