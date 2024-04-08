7fb72b77c537c5a5270eb39a41911ce5283c1395
profeBlas <profeBlas@localhost>
2024-04-08 16:26:45 -0300

    primera bis

--- TP2_EJ1/src/tp2_ej1/TP2_EJ1.java
+++ TP2_EJ1/src/tp2_ej1/TP2_EJ1.java
@@ -15,6 +15,7 @@ public class TP2_EJ1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println(Math.random());
     }
     
 }
