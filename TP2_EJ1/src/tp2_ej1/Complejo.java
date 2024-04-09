a033db35a8fb68e42d1f8869e85b3e09fdf0c3d2
Luis Mendoza <Luis Mendoza@YaTuSabe>
2024-04-09 20:51:51 -0300

    TP2_EJ1

--- TP2_EJ1/src/tp2_ej1/Complejo.java
+++ TP2_EJ1/src/tp2_ej1/Complejo.java
@@ -9,5 +9,98 @@ package tp2_ej1;
  * @author Luis Mendoza
  */
 public class Complejo {
+    private double re,im;   //Estos son los parametros del complejo
     
+    public Complejo(double re, double im) {
+        this.re = re; //Este es para asignar los dos parametros a la ves utilizando el"NEW"                 
+        this.im = im;
+    }
+    public Complejo(){ //Este es para no asignar valor a los parametros utilizando el "NEW"
+    }
+    
+    
+    
+    public double getRe() {  //Estos son para retornar los parametros real e imaginario
+        return re;           //Ej: para mostrar el parametro deseado
+    }
+
+    public double getIm() {
+        return im;
+    }
+
+    public void setRe(double re) { //Asigna el valor a la parte real
+        this.re = re;
+    }
+
+    public void setIm(double im) {  //Asigna el valor a la parte imaginaria
+        this.im = im;
+    }
+     //metodos de instancia
+    public void inc(Complejo a){
+        this.re += a.re;
+        this.im += a.im;
+    }
+    public void dec(Complejo a){
+        this.re -= a.re;
+        this.im -= a.im;
+    }
+    
+    public static Complejo modu(Complejo b){
+    Complejo a = new Complejo();
+    a.re = Math.sqrt(b.re* b.re);
+    a.im = Math.sqrt(b.im * b.im);
+    return a;
+    }
+    
+    public void conjugado(){
+    this.im = -this.im;
+    }
+    public void por(Complejo b){
+     this.re = (this.re * b.re)-(this.im*b.im);                       
+     this.im = (this.re * b.im)+(this.im * b.re);
+    }
+    public void div(Complejo b){
+     this.re = ((this.re * b.re)+(this.im * b.im)) / ((b.re * b.re)+(b.im * b.im));                       
+     this.im = ((this.im * b.re)-(this.re * b.im)) / ((b.re * b.re)+(b.im * b.im));
+    }
+    
+    //Metodos generales
+    
+    @Override
+    public String toString() {
+        return "COMPLEJO{" + "re=" + re + ", im=" + im + '}';
+    }
+    //METODOS DE CLASE
+    public static Complejo suma(Complejo a,Complejo b){
+        Complejo c = new Complejo();          //(a.re,a.im)  OTRA FORMA a C se le asigna A
+        c.re = a.re + b.re;                       //c.inc(b);   OTRA FORMA seSuma C+B
+        c.im = a.im + b.im;
+        return c;
+    }
+    
+    public static Complejo resta(Complejo a,Complejo b){
+        Complejo c = new Complejo();
+        c.re = a.re - b.re;                       
+        c.im = a.im - b.im;
+        return c;
+    }
+    
+    public static Complejo multiplicacion(Complejo a,Complejo b){
+        Complejo c = new Complejo();
+        c.re = (a.re * b.re)-(a.im*b.im);                       
+        c.im = (a.re * b.im)+(a.im * b.re);
+        return c;
+    }
+    
+    
+    public static Complejo divicion(Complejo a,Complejo b){
+        Complejo c = new Complejo();
+        c.re = ((a.re * b.re)+(a.im * b.im)) / ((b.re * b.re)+(b.im * b.im));                       
+        c.im = ((a.im * b.re)-(a.re * b.im)) / ((b.re * b.re)+(b.im * b.im)); 
+        return c;
+    }
+
+    boolean modulo() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
--- TP2_EJ1/src/tp2_ej1/TP2_EJ1.java
+++ TP2_EJ1/src/tp2_ej1/TP2_EJ1.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package tp2_ej1;
-
+import java.util.*;
 /**
  *
  * @author Luis Mendoza
@@ -14,7 +14,121 @@ public class TP2_EJ1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Complejo c; 
+        c = new Complejo();
+        c.setRe(2); 
+        c.setIm(5); 
+        System.out.println("Parte Real: " + c.getRe()); 
+        System.out.println("Parte Imaginaria: " + c.getIm()); 
+        
+        System.out.println("************************************************");
+        
+        Complejo a = new Complejo(3,10);
+        System.out.println("Parte Real: " + a.getRe());
+        System.out.println("Parte Imaginaria: " + a.getIm());
+        
+        System.out.println("************************************************");
+        
+        System.out.println("INC:");
+        Complejo in = new Complejo(2,4);
+        in.inc(a);
+        System.out.println(in);
+        
+        System.out.println("************************************************");
+        
+        System.out.println("DEC:");
+        Complejo de = new Complejo(4,7);
+        de.dec(a);
+        System.out.println(de);
+        
+        System.out.println("************************************************");
+        
+        System.out.println("MODULO:");
+        Complejo mod;
+        mod = Complejo.modu(a);
+        System.out.println(mod);
+        
+        System.out.println("************************************************");
+        
+        System.out.println("POR:");
+        Complejo p = new Complejo(6,9);
+        p.por(a);
+        System.out.println(p);
+        
+        System.out.println("************************************************");
+        
+        System.out.println("DIV:");
+        Complejo divi = new Complejo(3,4);
+        divi.div(a);
+        System.out.println(divi);
+        
+        System.out.println("************************************************");
+        
+        System.out.println("conjugado:");
+        Complejo con = new Complejo(5,8);
+        con.conjugado();
+        System.out.println(con);
+        
+        
+        
+        
+        
+        System.out.println("************************************************");
+        
+        
+        
+        
+        
+        //_____________________________________________
+        
+        System.out.println("SUMA:");
+        Complejo sum;
+        sum = Complejo.suma(a,c );
+        System.out.println(sum);  
+        
+        System.out.println("************************************************");
+        
+        System.out.println("RESTA:");
+        Complejo res;
+        res = Complejo.resta(a,c);
+        System.out.println(res);  
+        
+        System.out.println("************************************************");
+        
+        System.out.println("MULTIPLICACION:");
+        Complejo mul;
+        mul = Complejo.multiplicacion(a,c);
+        System.out.println(mul);  
+        
+        System.out.println("************************************************");
+        
+        System.out.println("DIVICION:");
+        Complejo di;
+        di = Complejo.divicion(a,c);
+        System.out.println(di);  
+        
+        
+        //________________________EJ:06-A)_________________________
+        
+        
+        int i,j;
+        Complejo v[] = new Complejo[10];
+        for(i=0;i<10;i++){
+           v[i] = new Complejo(Math.random()*100,Math.random()*100);
+        }
+        for(i=0;i<10;i++){
+            for(j=i+1;j<10;j++){
+               if(v[j].modulo() > v[i].modulo()){
+                   Complejo aux = v[i];
+                   v[i] = v[j];
+                   v[j] = aux;
+               }
+            }
+        
+        }
+        for(i=0;i<10;i++){
+            System.out.println(v[i]);
+        }
     }
     
 }
