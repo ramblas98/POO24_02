13f1ef889b57a8ff80af54310080328a116186c9
Naim Monteagudo <nicomonteagudo14@gmail.com>
2024-04-09 14:23:06 -0300

    1

--- TP2_EJ1/src/tp2_ej1/Complejo.java
+++ TP2_EJ1/src/tp2_ej1/Complejo.java
@@ -9,5 +9,40 @@ package tp2_ej1;
  * @author profeblas
  */
 public class Complejo {
+    private double rea, ima;
+    public Complejo(){
+        this.rea = this.ima = 0;
+    }
+    public Complejo(double rea, double ima){
+        this.rea = rea;
+        this.ima = ima;
+    }
+    public double getRea(){
+        return rea;
+    }
+    public double getIma(){
+        return ima;
+    }
+    public void setRea(double rea){
+        this.rea = rea;
+    }
+    public void setIma(double ima){
+        this.ima = ima;
+    }
+    public void inc(Complejo a){
+        this.rea += a.rea;
+        this.ima += a.ima;
+    }
+    public static Complejo suma(Complejo a, Complejo b){
+        Complejo c;
+        c.rea = a.rea + b.rea;
+        c.ima = a.ima + b.ima;
+        return c;
+    }
+    @Override
+    public String toString(){
+        return "Complejo{" + "rea=" + rea + ", ima=" + ima + '}';
+    }
+    
     
 }
