30972523d0f1a2acfd3d2ce22aa077d32bcad19b
Naim Monteagudo <nicomonteagudo14@gmail.com>
2024-04-10 01:06:21 -0300

    1

---
+++ TP2_EJ5/src/tp2_ej5/TP2_EJ5.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_ej5;
+
+public class TP2_EJ5 {
+    
+    public static void main(String[] args) {
+        Estudiante a = new Estudiante("Monteagudo", "Nicolas", 245, "nicomonteagudo14@gmail.com");
+        System.out.println(a);
+    }
+    
+}
