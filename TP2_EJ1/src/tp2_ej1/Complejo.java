e079433a939e93906a08a0b2288b715c087bdaf9
Naim Monteagudo <nicomonteagudo14@gmail.com>
2024-04-09 21:48:46 -0300

    3

--- TP2_EJ1/src/tp2_ej1/Complejo.java
+++ TP2_EJ1/src/tp2_ej1/Complejo.java
@@ -33,15 +33,70 @@ public class Complejo {
         this.rea += a.rea;
         this.ima += a.ima;
     }
+    public void dec(Complejo a){
+        this.rea -= a.rea;
+        this.ima -= a.ima;
+    }
+    public void por(Complejo a){
+        this.rea *= this.rea*a.rea - this.ima*a.ima;
+        this.ima *= this.rea*a.ima + this.ima*a.rea;
+    }
+    public void por(double a){
+        this.rea *= a;
+        this.ima *= a;
+        
+    }
+    public void div(Complejo a){
+        if(a.rea != 0 && a.ima !=0 ){
+            double deno =Math.pow(a.modulo(), 2);
+            this.rea = (this.rea*a.rea + this.ima*a.ima)/(deno);
+            this.ima = (this.ima*a.rea-this.rea*a.ima)/(deno);
+        }else System.out.println("No es posible dividir por cero");
+        
+    }
+    public void div(double a){
+        if(a!=0){
+            this.rea /= a;
+            this.ima /= a;
+        }else System.out.println("No es posible dividir por cero");
+    }
+    public double modulo(){
+        double modu;
+        modu = Math.sqrt(Math.pow(rea, 2)+Math.pow(ima, 2));
+        return modu;        
+    }
+    //met clases
     public static Complejo suma(Complejo a, Complejo b){
         Complejo c = new Complejo();
         c.rea = a.rea + b.rea;
         c.ima = a.ima + b.ima;
         return c;
     }
+    public static Complejo resta(Complejo a, Complejo b){
+        Complejo c = new Complejo();
+        c.rea = a.rea - b.rea;
+        c.ima = a.ima - b.ima;
+        return c;
+    }
+    public static Complejo producto(Complejo a, Complejo b){
+        Complejo c = new Complejo();
+        c.rea = a.rea*b.rea - a.ima*b.ima;
+        c.ima = a.rea*b.ima + a.ima*b.rea;
+        return c;
+    }
+    public static Complejo division(Complejo a, Complejo b){
+        Complejo c = new Complejo();
+        if(b.rea!=0 && b.ima!=0){
+            double deno = Math.pow(b.modulo(),2);
+            c.rea = ((a.rea*b.rea)+(a.ima*b.ima))/deno;
+            c.ima = (a.ima*b.rea-a.rea*b.ima)/deno;
+        }else System.out.print("No es posible dividir por cero");    
+        return c;
+    }
+    
     @Override
     public String toString(){
-        return "Complejo{" + "rea=" + rea + ", ima=" + ima + '}';
+        return "Complejo{" + "rea= " + rea + ", ima= " + ima + '}';
     }
     
     
