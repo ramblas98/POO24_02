601ecd65d68808b4b90b508173ee5401a146de7d
Naim Monteagudo <nicomonteagudo14@gmail.com>
2024-04-10 01:10:37 -0300

    1

---
+++ TP2_EJ3/src/tp2_ej3/TP2_EJ3.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_ej3;
+
+/**
+ *
+ * @author Bruno
+ */
+public class TP2_EJ3 {
+
+    /**
+    public static void main(String[] args) {
+    }
+    
+}
