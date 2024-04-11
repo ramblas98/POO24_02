2534b17e03c4f66804756553beeec72ebbc033eb
Naim Monteagudo <nicomonteagudo14@gmail.com>
2024-04-10 21:41:42 -0300

    2

--- TP2_EJ3/src/tp2_ej3/TP2_EJ3.java
+++ TP2_EJ3/src/tp2_ej3/TP2_EJ3.java
@@ -8,6 +8,34 @@ public class TP2_EJ3 {
 
     
     public static void main(String[] args) {
+        int s=0;
+        ListaEnlazada a = new ListaEnlazada();
+        System.out.print("lista con agregar mayor:");
+        a.agregarMayor(24);
+        a.agregarMayor(5);
+        a.agregarMayor(7);
+        a.agregarMayor(3);
+        a.agregarMayor(-2);
+        a.agregarMayor(24);
+        a.mostrar();
+        
+        System.out.print("\nlista con agregar mayor despues de eliminar 24:");
+        s = a.eliminar(24);
+        a.mostrar();
+        System.out.print("\nSe eliminaron "+s+" nodos.");
+        System.out.print("\nlista despues de reemplar el 7 por el 5:");
+        a.cambiarValor(7, 5);
+        a.mostrar();
+        System.out.print("\nlista con agregar menor:");
+        ListaEnlazada b = new ListaEnlazada();
+        b.agregarMenor(27);
+        b.agregarMenor(23);
+        b.agregarMenor(24);
+        b.agregarMenor(29);
+        b.agregarMenor(395);
+        b.agregarMenor(-2);
+        b.mostrar();
+        System.out.print("\nlista con agregar menor:");
         
     }
     
